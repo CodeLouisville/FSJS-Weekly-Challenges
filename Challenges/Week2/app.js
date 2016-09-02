012395cf4c2306dc87144a9a6e57aa30404932a9
Jordin Marshall <jmarshall03@bellarmine.edu>
2016-09-02 11:24:22 -0400

    Turned into anonymous function

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -9,17 +9,25 @@
 var $correct = false;
 alert("Greetings friend!");
 
-while(!$correct){
+var $input = function(){
+    $guess = 0;
+    while(!$correct){
     $userInput = prompt("We sincerely request your valuable input: ");
     $response = "HELLO";
         if($userInput.toUpperCase() == $response){
             alert($userInput + " World");
             $correct = true;
+            console.log("Guess:" + $guess);
         } else{
             alert("Sorry, that is not the valuable input we were hoping for:");
-            var $tryAgain = prompt("Would you like to try again: yes or no?");
+            var $tryAgain = prompt("Would you like to quit: yes or no?");
+            $guess++;
             if($tryAgain.toUpperCase() == 'NO' || $tryAgain.toUpperCase() == 'N' ){
                 break;
             }
-        }
+        }        
+    }
+    return $guess;
 }
+
+$input();

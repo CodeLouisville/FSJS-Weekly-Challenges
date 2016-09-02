f7653c18f7248c1dc0676e9943b88e060d1175ea
Jordin Marshall <jmarshall03@bellarmine.edu>
2016-09-01 20:40:27 -0400

    Week 2 code challenge complete.

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,20 @@
 //Call function
 
 //Alert user results
+var $correct = false;
+alert("Greetings friend!");
+
+while(!$correct){
+    $userInput = prompt("We sincerely request your valuable input: ");
+    $response = "HELLO";
+        if($userInput.toUpperCase() == $response){
+            alert($userInput + " World");
+            $correct = true;
+        } else{
+            alert("Sorry, that is not the valuable input we were hoping for:");
+            var $tryAgain = prompt("Would you like to try again: yes or no?");
+            if($tryAgain.toUpperCase() == 'NO' || $tryAgain.toUpperCase() == 'N' ){
+                break;
+            }
+        }
+}

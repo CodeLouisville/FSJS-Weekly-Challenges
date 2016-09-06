aea971a5f434e630dff4b3b0678a542d7de5068d
xiaojinglin <candylinxiaojing@gmail.com>
2016-09-06 17:35:22 +0000

    finish the challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,11 @@
-//create function
-  //control and return statements
+function hello(input){//create function
+  if(input=="Hello") 
+    return "Hello World!";
+  else return "Incorrect input!";
+}//control and return statements
 
-//Prompt user for input and store variable
+var user_enter=prompt("Please enter a word!");//Prompt user for input and store variable
 
-//Call function
+var i=hello(user_enter);//Call function
 
-//Alert user results
+alert(i);//Alert user results

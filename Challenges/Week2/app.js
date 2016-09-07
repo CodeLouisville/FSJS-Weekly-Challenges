77368b594ecb10fc9f5b7b28a80d0cee4ca22642
crystole <crystole.roberts@gmail.com>
2016-09-07 19:52:22 -0400

    my first commit

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,19 @@
 //Call function
 
 //Alert user results
+
+function processMs(msg) {
+    if(msg === "Hello") {
+        alert("Hello World");
+    }
+    
+    else
+        alert("You didn't say hello");
+    
+    console.log(msg);
+}
+
+var message;
+message=prompt();
+
+processMs(message);

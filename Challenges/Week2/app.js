01ba84ba8882ec25438b7888d4175e97f255ea05
benblucas <lucas.banet@gmail.com>
2016-09-08 19:47:02 -0400

    completing week 2 JS challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -3,6 +3,30 @@
 
 //Prompt user for input and store variable
 
+function processInput(message)
+{
+
+  console.log(message);
+
+  if(message === "Hello!")
+  {
+    return("Hello world!");
+
+  }
+  else {
+    return("You didn't say hello :(");
+  }
+}
+
+var userInput;
+
+userInput = prompt();
+
+var response = processInput(userInput);
+
+
+
+
 //Call function
 
 //Alert user results

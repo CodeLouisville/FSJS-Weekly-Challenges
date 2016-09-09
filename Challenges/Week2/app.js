2bc3a2252ffb660b4b5be273d43b0cdbae7f1fa9
TheScogg <thescogg@gmail.com>
2016-09-08 23:17:09 -0400

    Week 2 Code Challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,17 @@
 //create function
   //control and return statements
-
+function talk(input) {
+    if (input === "Hello") {
+        return "Hello World";
+    } else {
+        return "I'm sorry, I don't understand you.";
+    }
+}
 //Prompt user for input and store variable
+var input = prompt("Tell me something!");
 
 //Call function
+var response = talk(input);
 
 //Alert user results
+alert(response);

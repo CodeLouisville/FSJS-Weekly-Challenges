110144fedcfad19ff2f65da0f34597de7bd7fe5e
ShauvonM <smcgill@denyconformity.com>
2016-09-14 18:09:28 -0400

    My challenge that should be good for week 4

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,29 +1,19 @@
-$('#menu a').tooltip();
+function openBlock (id) {
+    var block = document.getElementById(id);
+    var blocks = document.getElementById("main_content").getElementsByTagName("div");
 
-$('.tooltip-left').tooltip({
-    direction: "left"
-});
+    for (var y = 0; y < blocks.length; y++) {
+        blocks[y].style.display = "none";
+    }
+    block.style.display = "block";
+    
+    // TODO: remove the "active" class from all of the li elements inside the menu
+    // TODO: add the "active" class to the li element that contains the link that was clicked
 
-$('.tooltip-below').tooltip({
-    direction: "below"
-});
+}
 
-$('.tooltip-above').tooltip({
-    direction: "above"
-});
+// TODO: add the "hover" class to the menu items when you hover over them
 
-$('.tooltip-delay-500').tooltip({
-    delay: 500
-});
 
-$('.tooltip-delay-2000').tooltip({
-    delay: 2000
-});
+// TODO: set up the tooltip plugin on all of the links in the menu
 
-$('.tooltip-data-attribute').tooltip();
-
-$('.tooltip-custom-text').tooltip({
-    title: "This was passed as an option in JavaScript!"
-});
-
-$('.tooltip-title').tooltip();
--- Challenges/Week4/SolutionFiles/js/main.js
+++ Challenges/Week4/SolutionFiles/js/main.js
@@ -1,29 +1,31 @@
-$('#menu a').tooltip();
+function openBlock (id) {
+    var block = document.getElementById(id);
+    var blocks = document.getElementById("main_content").getElementsByTagName("div");
 
-$('.tooltip-left').tooltip({
-    direction: "left"
+    for (var y = 0; y < blocks.length; y++) {
+        blocks[y].style.display = "none";
+    }
+    block.style.display = "block";
+    
+    // TODO: remove the "active" class from all of the li elements inside the menu
+    // TODO: add the "active" class to the li element that contains the link that was clicked
+    var link = document.getElementById("menu_item_" + id);
+    var listItems = document.getElementById("menu").getElementsByTagName("li");
+
+    for (var x = 0; x < listItems.length; x++) {
+        listItems[x].className = "";
+    }
+    link.parentNode.className = "active";
+}
+
+// TODO: add the "hover" class to the menu items when you hover over them
+$('#menu a').hover(function () {
+        $(this).addClass('hover');
+    }, function () {
+        $(this).removeClass('hover');
+    });
+
+// TODO: set up the tooltip plugin on all of the links in the menu
+$('#menu a').tooltip({
+    delay: 1000
 });
-
-$('.tooltip-below').tooltip({
-    direction: "below"
-});
-
-$('.tooltip-above').tooltip({
-    direction: "above"
-});
-
-$('.tooltip-delay-500').tooltip({
-    delay: 500
-});
-
-$('.tooltip-delay-2000').tooltip({
-    delay: 2000
-});
-
-$('.tooltip-data-attribute').tooltip();
-
-$('.tooltip-custom-text').tooltip({
-    title: "This was passed as an option in JavaScript!"
-});
-
-$('.tooltip-title').tooltip();

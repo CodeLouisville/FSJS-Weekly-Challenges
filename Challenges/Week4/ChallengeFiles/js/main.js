6284a9e38784f242acc3182b9933bf4c16c54c72
Michael Christy <emonomics@gmail.com>
2016-09-20 19:04:45 -0400

    got hover function working

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -10,10 +10,24 @@ function openBlock (id) {
     // TODO: remove the "active" class from all of the li elements inside the menu
     // TODO: add the "active" class to the li element that contains the link that was clicked
 
-}
+};
+
+
+$("#menu li a").click(
+    console.log("yo dawg!");
+    openBlock(id);
+)
 
 // TODO: add the "hover" class to the menu items when you hover over them
 
+$("#menu li a").hover(
+function() {
+    $(this).addClass("hover");
+    console.log ("im being added");
+  }, function() {
+    $(this).removeClass("hover");
+  }
+);
 
 // TODO: set up the tooltip plugin on all of the links in the menu
 

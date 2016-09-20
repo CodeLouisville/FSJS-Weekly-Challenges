f51aa334d19ab5edeedb343af093f4a1c6329d55
xiaojinglin <candylinxiaojing@gmail.com>
2016-09-20 09:19:17 -0400

    finish week4 challenge

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,3 +1,4 @@
+var list=document.getElementById("menu");
 function openBlock (id) {
     var block = document.getElementById(id);
     var blocks = document.getElementById("main_content").getElementsByTagName("div");
@@ -6,14 +7,29 @@ function openBlock (id) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
-    // TODO: remove the "active" class from all of the li elements inside the menu
-    // TODO: add the "active" class to the li element that contains the link that was clicked
 
+    // TODO: remove the "active" class from all of the li elements inside the menu
+    var listActive=list.getElementsByClassName("active");
+        listActive[0].removeAttribute("class")
+
+    // TODO: add the "active" class to the li element that contains the link that was clicked
+        var anchor= document.getElementById("menu_item_"+id);
+        var listClicked= anchor.parentNode;
+        listClicked.className="active";
 }
 
-// TODO: add the "hover" class to the menu items when you hover over them
+//Add a onclick event to the link
+var link=document.getElementsByTagName("a");
+for(i=0;i<link.length;i++){
+  link[i].onclick=function(){
+    var blockId=this.id.substring(10);//The id which was clicked is menu_item_+blockId
+    openBlock(blockId);
+  };
 
+  // TODO: add the "hover" class to the menu items when you hover over them
+  link[i].onmouseover=function(){this.className="hover";};
+  link[i].onmouseout=function(){this.removeAttribute("class");};
+}
 
 // TODO: set up the tooltip plugin on all of the links in the menu
-
+$('a').tooltip();

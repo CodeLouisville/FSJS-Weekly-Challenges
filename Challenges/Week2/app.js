ece6495a93bfcfe74ceb21ea140de77a22cb29dc
Lone Scholar <Lone Scholar>
2016-09-21 20:59:58 -0400

    completed user Prompt CHallenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,11 @@
 //Call function
 
 //Alert user results
+
+var input = prompt('Say Hello?');
+
+if (input.toLocaleLowerCase() === 'hello') {
+	alert('Hello World');
+} else {
+	alert('You didnt say Hello...');
+}

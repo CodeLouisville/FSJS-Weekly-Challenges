1080ab4c7364b6d711535a2e143f2dda598da7a5
Michael Christy <emonomics@gmail.com>
2016-10-04 18:01:47 -0400

    remove the old list of 20 pokemon when clicking next or prev

--- Challenges/Week5/js/app.js
+++ Challenges/Week5/js/app.js
@@ -1,11 +1,3 @@
-//function addPokemon(name) {
-//    $(`
-//        <li class="poke-card">
-//            <h3 class="name">${name}</h3>
-//        </li>
-//    `).appendTo('#pokemon');
-//};
-
 var pokedex = "http://pokeapi.co/api/v2/pokemon/";
 var pokedexNext;
 var pokedexPrev;
@@ -19,20 +11,19 @@ $.getJSON(pokedex, function addPokemon(pokeName) {
     pokedexPrev = pokeName.previous;
 });
 
-
 // 2.2) When a user clicks next or previous, remove all existing Pokémon from the ul element and add the new list of Pokémon. 
 $("#previous").click(function () {
-    console.log("prev clicked");
+    $('.poke-card').remove();
     $.getJSON(pokedexPrev, function addPokemon(pokeName) {
         for (i = 0; i < pokeName.results.length; i++)
             $(`<li class="poke-card"><h3 class="name">${pokeName.results[i].name}</h3></li>`).appendTo('#pokemon');
         pokedexNext = pokeName.next;
         pokedexPrev = pokeName.previous;
-    });
-    // pokeName.previous;
+    }); 
 });
 
 $("#next").click(function () {
+    $('.poke-card').remove();
     $.getJSON(pokedexNext, function addPokemon(pokeName) {
         for (i = 0; i < pokeName.results.length; i++)
             $(`<li class="poke-card"><h3 class="name">${pokeName.results[i].name}</h3></li>`).appendTo('#pokemon');
@@ -40,6 +31,3 @@ $("#next").click(function () {
         pokedexPrev = pokeName.previous;
     });
 });
-// 2.1) Use the "next" and "previous" properties of the pokemon resource object to get the next or previous list of Pokémon.
-
-// 2.3) Use jQuery to disable the next/previous buttons if there are no more Pokémon to retrieve in that direction.
